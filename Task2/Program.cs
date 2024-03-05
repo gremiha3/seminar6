c3a44ccb5fb2bb351b92c1c699f463e2fcea0505
Nikolay Kozlov <gremiha3@gmail.com>
2024-03-05 14:23:04 +0300

    Task2 is done

---
+++ Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 2: Задайте строку, содержащую латинские буквы
+// в обоих регистрах. Сформируйте строку, в которой все
+// заглавные буквы заменены на строчные. 
+
+// “aBcD1ef!-” => “abcd1ef!-” 
+
+string upCaseToLowCase(string src)
+{
+    return src.ToLower();
+}
+
+string src = "aBcD1ef!";
+
+Console.WriteLine(upCaseToLowCase(src));
