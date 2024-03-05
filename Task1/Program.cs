4ee8dade49a739a2a28fadb8e760edef70d460b0
Nikolay Kozlov <gremiha3@gmail.com>
2024-03-05 14:06:50 +0300

    Task1 is done

---
+++ Task1/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 1: Задайте двумерный массив символов (тип char
+// [,]). Создать строку из символов этого массива. 
+
+// a b c => “abcdef”
+// d e f 
+
+string createStringFromMatrix(char[,] matrix)
+{
+    string str = "";
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            str += matrix[i, j];
+        }
+    }
+    return str;
+}
+
+void printMatrix(char[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i,j]}\t");
+        }
+        Console.WriteLine();
+    }
+
+}
+
+
+char[,] charMatrix = {
+  {'a','b','c'},
+  {'d','e','f'}
+};
+printMatrix(charMatrix);
+Console.WriteLine();
+Console.WriteLine(createStringFromMatrix(charMatrix));
+
